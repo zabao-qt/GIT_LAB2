10ad348f0422b68b104855553b3f3cdb707da6b0
TA GIA BAO <tagiabao.qt@gmail.com>
2023-11-01 19:03:29 +0700

    exercise 7: update source code

--- Timer Interrupt and LED Scanning/Core/Src/main.c
+++ Timer Interrupt and LED Scanning/Core/Src/main.c
@@ -62,8 +62,6 @@ int timer1_counter = 0;
 int timer1_flag = 0;
 int timer2_counter = 0;
 int timer2_flag = 0;
-int timer3_counter = 0;
-int timer3_flag = 0;
 int TIMER_CYCLE = 10; // 10ms
 void setTimer0(int duration) {
 	timer0_counter = duration / TIMER_CYCLE;
@@ -77,27 +75,19 @@ void setTimer2(int duration) {
 	timer2_counter = duration / TIMER_CYCLE;
 	timer2_flag = 0;
 }
-void setTimer3(int duration) {
-	timer3_counter = duration / TIMER_CYCLE;
-	timer3_flag = 0;
-}
 void timer_run() {
 	if (timer0_counter > 0) {
 		timer0_counter--;
 		if (timer0_counter == 0) timer0_flag = 1;
 	}
 	if (timer1_counter > 0) {
-			timer1_counter--;
-			if (timer1_counter == 0) timer1_flag = 1;
-		}
+		timer1_counter--;
+		if (timer1_counter == 0) timer1_flag = 1;
+	}
 	if (timer2_counter > 0) {
-			timer2_counter--;
-			if (timer2_counter == 0) timer2_flag = 1;
-		}
-	if (timer3_counter > 0) {
-			timer3_counter--;
-			if (timer3_counter == 0) timer3_flag = 1;
-		}
+		timer2_counter--;
+		if (timer2_counter == 0) timer2_flag = 1;
+	}
 }
 /* USER CODE END 0 */
 
